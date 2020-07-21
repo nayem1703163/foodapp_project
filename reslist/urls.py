337355541a75115e26e744a4be9afeb2883ee389
sinayem <nayem1703163@users.noreply.github.com>
2020-07-22 01:50:12 +0600

    some des change

--- reslist/urls.py
+++ reslist/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 import foodlist.views 
 urlpatterns = [
-    path('',views.resview),
+    path('',views.resview,name="reslist"),
     path('<int:res_id>/',foodlist.views.foodview,name="resdetail"),
 ]
