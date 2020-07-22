478d5e4203938cce0e9575c9174e05def194f6eb
Nahid <nazm.nahid@gmail.com>
2020-07-22 18:18:26 +0600

    First Task Done

--- User/views.py
+++ User/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 
 # Create your views here.
 def signup_user(request):
-    form = UserCreationForm()
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #loged in
+            return redirect("reslist")
+    else:
+        form = UserCreationForm()
     return render(request,"User/signup.html",{"form":form})
 def login_user(request):
-    return render(request,'user/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+           # form.save()
+            # loged in
+            return redirect("reslist")
+    else:
+        form = AuthenticationForm()
+    return render(request, "User/login.html", {"form": form})
--- reslist/urls.py
+++ reslist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from . import views
-import foodlist.views 
+import foodlist.views
+
 urlpatterns = [
     path('',views.resview,name="reslist"),
     path('<int:res_id>/',foodlist.views.foodview,name="resdetail"),
