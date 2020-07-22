ca56ee0c847c7693b05139ae1c35d3ee5ac96141
sinayem <nayem1703163@users.noreply.github.com>
2020-07-22 22:49:46 +0600

    'new'

--- User/url.py
+++ User/url.py
@@ -3,10 +3,6 @@ from .import views
 
 urlpatterns =[
     url(r'^login/$',views.login_user, name='login_user'),
-<<<<<<< HEAD
     url(r'^logout/$',views.logout_user, name='logout'),
-=======
-    url(r'^logout/$',views.logout_user, name='logout_user'),
->>>>>>> e1806eb2a5c959dadc82846c06cb49e9aecd7797
     url(r'^signup/$',views.signup_user, name='signup_user'),
 ]
