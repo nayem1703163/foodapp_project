f4ca4de01f8e1dcc319f75e08cadedf52102851d
Nahid <nazm.nahid@gmail.com>
2020-07-22 16:32:05 +0600

    changed?

--- User/views.py
+++ User/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-#Changed Nahiddddd
-#Again
-#Abar
+def signup_user(request):
+    return render(request,"User/signup.html")
+def login_user(request):
+    return render(request,'user/login.html')
