3b6210a3a6508ffdb046031ec924cac5bd0c7049
Nahid <nazm.nahid@gmail.com>
2020-07-22 19:59:50 +0600

    First Task Done :D

--- User/views.py
+++ User/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_user(request):
     if request.method=='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            #loged in
+            user_= form.save()
+            login(request,user_)
             return redirect("reslist")
     else:
         form = UserCreationForm()
     return render(request,"User/signup.html",{"form":form})
+
+
 def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # loged in __
+            user_ = form.get_user()
+            login(request,user_)
             return redirect("reslist")
     else:
         form = AuthenticationForm()
     return render(request, "User/login.html", {"form": form})
+
+def logout_user(request):
+    if request.method == 'POST':
+         logout(request)
+    return redirect("reslist")
+
