e1806eb2a5c959dadc82846c06cb49e9aecd7797
Nahid <nazm.nahid@gmail.com>
2020-07-22 21:53:04 +0600

    Logout done

--- User/url.py
+++ User/url.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns =[
     url(r'^login/$',views.login_user, name='login_user'),
-    url(r'^logout/$',views.login_user, name='logout_user'),
-    url(r'^signup/$',views.signup_user, name='signup_user')
+    url(r'^logout/$',views.logout_user, name='logout_user'),
+    url(r'^signup/$',views.signup_user, name='signup_user'),
 ]
