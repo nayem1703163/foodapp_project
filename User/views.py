99506f5027d196f683e8b464305f23b3d55c94db
Nahid <nazm.nahid@gmail.com>
2020-07-22 16:43:59 +0600

    changed?

--- User/views.py
+++ User/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_user(request):
-    return render(request,"User/signup.html")
+    form = UserCreationForm()
+    return render(request,"User/signup.html",{"form":form})
 def login_user(request):
     return render(request,'user/login.html')
