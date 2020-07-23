c19444a81ecb1b75045e18f82efbe4be8bcd3d29
Nahid <nazm.nahid@gmail.com>
2020-07-23 21:22:41 +0600

    Required Login

--- User/views.py
+++ User/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth import login, logout
 
-# Create your views here.
+# Create your views here.____
 def signup_user(request):
     if request.method=='POST':
         form = UserCreationForm(request.POST)
