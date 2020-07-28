e9bb6a36bc1de57744f47997b609a5bd08fa17fb
Nazmul Hossain Nahid <54554725+Nazm-nahid@users.noreply.github.com>
2020-07-28 22:42:20 +0600

    Delete url.py

--- User/url.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .import views
-
-urlpatterns =[
-    url(r'^login/$',views.login_user, name='login_user'),
-    url(r'^logout/$',views.logout_user, name='logout'),
-    url(r'^signup/$',views.signup_user, name='signup_user'),
-]
